ad31c30105bbcf4c9fa30335e6c3510f4b8be99a
Harsh chaudhary <146811538+piyush1740x@users.noreply.github.com>
2025-01-27 09:31:57 +0530

    Update FactorAvailability.jsx

--- src/FactorAvailability.jsx
+++ src/FactorAvailability.jsx
@@ -8,7 +8,7 @@ const FactorAvailability = () => {
     {
       name: "BRD Medical College",
       location: "गोरखपुर",
-      factors: { factor8: true, factor9: true, factor7: false },
+      factors: { factor8: true, factor9: true, factor7: true },
     },
     {
       name: "District Hospital",
